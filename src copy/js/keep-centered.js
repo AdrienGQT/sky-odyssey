1a6e2e72aec50783ad87feaf3a997b0a04aef775
AdrienGQT 🤸 <adrien.gsqt@gmail.com>
2025-02-02 12:07:36 +0100

    Remove deprecated folder

--- src copy/js/keep-centered.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('parallax').style.transform = `translate(${-(document.getElementById('parallax').offsetWidth - document.getElementById('landing').offsetWidth) / 2}px,0px)`;
-
-addEventListener("resize", (event) => {
-    document.getElementById('parallax').style.transform = `translate(${-(document.getElementById('parallax').offsetWidth - document.getElementById('landing').offsetWidth) / 2}px,0px)`;
-});
--- src copy/js/start.js
+++
@@ -1,7 +0,0 @@
-function start(){
-    window.scrollTo(0, document.body.scrollHeight);
-    // html.style.scrollBehavior = "smooth"
-    document.getElementById('first').style.display = "none";
-    body.style.overflowY = "scroll";
-
-}
